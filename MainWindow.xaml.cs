f348219190a9427259a249174c095fbfab3074b6
Linearthrone <213573343+Linearthrone@users.noreply.github.com>
2025-12-24 20:29:58 -0500

    2.2.3

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -368,10 +368,4 @@ namespace LLMOverlay
             }
         }
     }
-
-    public class ChatMessage {
-        public string Role { get; set; } = "";
-        public string Content { get; set; } = "";
-        public DateTime Timestamp { get; set; } = DateTime.Now;
-    }                
 }
